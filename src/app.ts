82193accad1bb3c2ceea88bd760c10038401516c
Franco Morroni <fmorroni99@gmail.com>
2023-05-05 17:51:35 -0300

    Add ts files to server response for debugging

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,5 @@ app.listen(5000, () => {
 
 app.use(express.static('dist/public'))
 
-// app.use(express.json());
-app.post('/postEndpoint', (req, res) => {
-  let s = ''
-  req.on('data', chunk => s += chunk)
-  req.on('end', () => console.log(s))
-  res.end('chau')
-})
+// For debuging
+app.use('/src/public/', express.static('src/public'))
